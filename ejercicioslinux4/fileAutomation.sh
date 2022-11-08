f452c30f8502ef5a532695bdbaa51443c8d13044
Nahartor <nicobalbonipalma@gmail.com>
2022-11-08 22:49:23 +0100

    Trabajo casa 08/11, no he avanzado nada

--- ejercicioslinux4/fileAutomation.sh
+++ ejercicioslinux4/fileAutomation.sh
@@ -5,7 +5,48 @@
 #Ejercicios de BASH LINUX 4
 #Ejercicio 1
 
-usuarios=$(cat ./files.txt | awk -F: '{print $4}')
+#Definimos funciones
+function checkUser(){
+    IFS=":"
+    while read -r $tipo $archivo $permisos $usuario $grupo
+    do
+        num=$(cat /etc/passwd | grep "^$usuario:" | wc -l)
+        if [ $num -ge 1 ];then
+            echo "El usuario existe"
 
-echo $usuarios
+        else
+            echo "El usuario no existe"
+        fi
 
+    done < files.txt
+}
+
+function checkGroup(){
+    IFS=":"
+    while read -r $tipo $archivo $permisos $usuario $grupo
+    do
+        num2=$(cat /etc/passwd | grep "^$grupo" | wc -l)
+        if [ $num2 -ge 1 ];then
+            echo "El grupo existe"
+
+        else
+            echo "El grupo no existe"
+
+        fi
+
+    done < files.txt
+}
+
+function checkRoot(){
+    if [ $(id -u $USER) -ne 0 ];then
+        echo "Este script debe ser ejecutado como root"
+        return 1
+
+    else
+        return 0
+
+    fi
+}
+
+#Comienzo del script
+checkRoot
