9bc203bbfc41c3431bad796911baaf2388f5b0b8
Nahartor <nicobalbonipalma@gmail.com>
2022-11-09 22:09:55 +0100

    Trabajo casa 09/11

--- ejercicioslinux4/fileAutomation.sh
+++ ejercicioslinux4/fileAutomation.sh
@@ -7,46 +7,79 @@
 
 #Definimos funciones
 function checkUser(){
-    IFS=":"
-    while read -r $tipo $archivo $permisos $usuario $grupo
+    while read -r linea
     do
-        num=$(cat /etc/passwd | grep "^$usuario:" | wc -l)
+        usuario=$(echo $linea | awk -F':' '{print $4}')
+        num=$(cat /etc/passwd | grep -i "^$usuario:" | wc -l)
         if [ $num -ge 1 ];then
-            echo "El usuario existe"
+            echo "El usuario \"$usuario\" existe"
+            echo "$(date)--- El usuario \"$usuario\" existe" >> fileAutomation.log
 
         else
-            echo "El usuario no existe"
+            echo "El usuario \"$usuario\" no existe"
+            echo "$(date)--- El usuario \"$usuario\" no existe" >> fileAutomation.log
+
         fi
 
     done < files.txt
 }
 
 function checkGroup(){
-    IFS=":"
-    while read -r $tipo $archivo $permisos $usuario $grupo
+    while read -r linea
     do
-        num2=$(cat /etc/passwd | grep "^$grupo" | wc -l)
+        grupo=$(echo $linea | awk -F':' '{print $5}')
+        num2=$(cat /etc/passwd | grep -i "^$grupo" | wc -l)
         if [ $num2 -ge 1 ];then
-            echo "El grupo existe"
+            echo "El grupo \"$grupo\" existe"
+            echo "$(date)--- El grupo \"$grupo\" existe" >> fileAutomation.log
 
         else
-            echo "El grupo no existe"
+            echo "El grupo \"$grupo\" no existe"
+            echo "$(date)--- El grupo \"$grupo\" no existe" >> fileAutomation.log
 
         fi
 
     done < files.txt
 }
 
+
 function checkRoot(){
     if [ $(id -u $USER) -ne 0 ];then
         echo "Este script debe ser ejecutado como root"
-        return 1
-
-    else
-        return 0
+        echo "$(date)--- Este script debe ser ejecutado como root" >> fileAutomation.log
+        exit 1
 
     fi
 }
 
+function creaAoD(){
+    while read -r linea
+    do
+    lectura=$(echo $linea | awk -F':' '{print $1}')
+        if [ "$lectura" == "d" ];then
+            directorio=$(echo $linea | awk -F':' '{print $2}')
+            mkdir $directorio
+            echo "El directorio ha sido creado satisfactoriamente en la ruta especificada: $directorio"
+            echo "$(date)--- El directorio ha sido creado satisfactoriamente en la ruta especificada: $directorio" >> fileAutomation.log
+
+
+        elif [ "$lectura" == "f" ];then
+            archivo=$(echo $linea | awk -F':' '{print $2}')
+            touch $archivo
+            echo "El archivo ha sido creado satisfactoriamente en la ruta especificada: $archivo"
+            echo "$(date)--- El archivo ha sido creado satisfactoriamente en la ruta especificada: $archivo" >> fileAutomation.log
+
+        fi
+    done < files.txt
+}
+
 #Comienzo del script
+echo "Chequeamos Root"
 checkRoot
+echo "Chequeamos Usuario"
+checkUser
+echo "Chequeamos Grupo"
+checkGroup
+echo "Creamos Archivos y coses"
+creaAoD
+
